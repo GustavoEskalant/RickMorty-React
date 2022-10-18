d3816b4e423a05f632033893c6823c470d95bcd2
Gustavo Eskalant <gustavo.esc.stgo@gmail.com>
2022-10-17 23:20:07 -0500

    Curso en react
    Proyecto de Rick and Morty en React

---
+++ src/App.js
@@ -0,0 +1,38 @@
+import logo from './logo.svg';
+import imageHome from './images/rick-morty.png';
+import './App.css';
+import { useState } from 'react';
+import Character from './components/Characters';
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character");
+    const characterApi = await api.json();
+
+    //console.log(characterApi);
+    setCharacters(characterApi.results);
+  }
+
+  //console.log(characters);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? <Character characters={characters} setCharacters={setCharacters} /> :
+          <>
+            <img src={imageHome} alt='Rick & Morty' className='img-home' />
+            <button onClick={reqApi} className='btn-search'>Buscar personajes</button>
+          </>
+        }
+
+
+
+      </header>
+    </div>
+  );
+}
+
+export default App;
